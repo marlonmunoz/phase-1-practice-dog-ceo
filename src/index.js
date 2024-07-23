c2ec86361711afb57123e6eab794c5aeee37772b
Marlon Munoz <marlon@marlonshade.com>
2024-07-23 04:42:20 -0400

    kinda com

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () {
+    fetch('https://dog.ceo/api/breeds/image/random/10')
+      .then(response => response.json())
+      .then(data => {
+        const imageContainer = document.getElementById('dog-image-container');
+        data.message.forEach(imageUrl => {
+          const imgElement = document.createElement('img');
+          imgElement.src = imageUrl;
+          imageContainer.appendChild(imgElement);
+        });
+      })
+  });
+
+
+
+
+
+
